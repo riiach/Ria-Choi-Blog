e22ddaf43ca9bd4e82e8cbd8dd3c013ce109348c
Ria Choi <riiachoii@gmail.com>
2026-02-07 21:30:27 +0900

    cors added

--- server/linkUrl.js
+++ server/linkUrl.js
@@ -8,7 +8,7 @@ app.use(
     cors({
         origin: [
             'http://localhost:5173',     // local dev
-            'https://your-frontend.onrender.com' // production frontend
+            'https://riachoiblog.riachoi.com' // production frontend
         ],
         methods: ['GET'],
     })
